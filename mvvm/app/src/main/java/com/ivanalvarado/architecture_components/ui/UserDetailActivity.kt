5e47327ba0b42ebdf26621abee1dbefcd01d3597
Ivan Alvarado <ivalvara21@gmail.com>
2019-05-31 20:31:31 -0700

    set up ui widgets for user detail

--- mvvm/app/src/main/java/com/ivanalvarado/architecture_components/ui/UserDetailActivity.kt
+++ mvvm/app/src/main/java/com/ivanalvarado/architecture_components/ui/UserDetailActivity.kt
@@ -1,9 +1,11 @@
 package com.ivanalvarado.architecture_components.ui
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.ivanalvarado.architecture_components.R
 import com.ivanalvarado.architecture_components.viewmodel.UserDetailViewModel
 import dagger.android.AndroidInjection
@@ -17,6 +19,12 @@ class UserDetailActivity : AppCompatActivity() {
     internal lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var userDetailViewModel: UserDetailViewModel
 
+    // UI Widgets
+    private lateinit var userNameTextView: TextView
+    private lateinit var userReputationTextView: TextView
+    private lateinit var userLocationTextView: TextView
+    private lateinit var userTypeTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
 
@@ -27,13 +35,17 @@ class UserDetailActivity : AppCompatActivity() {
 
         userDetailViewModel = ViewModelProviders.of(this, viewModelFactory).get(UserDetailViewModel::class.java)
         userDetailViewModel.setUserId(userId)
+        fetchUserDetail()
     }
 
     private fun setUpUi() {
-
+        userNameTextView = findViewById(R.id.user_detail_name_text_view)
+        userReputationTextView = findViewById(R.id.user_detail_reputation_text_view)
+        userLocationTextView = findViewById(R.id.user_detail_location_text_view)
+        userTypeTextView = findViewById(R.id.user_detail_type_text_view)
     }
 
     private fun fetchUserDetail() {
-
+        userDetailViewModel.getUserDetail().observe(this, Observer { TODO("Display UserDetail") })
     }
 }
